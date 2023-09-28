b50fdb7b62ff23aa4956008f4f24d396af37c8b7
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2023-09-28 20:57:28 -0300

    Quanto commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import board_game.Board;
-import board_game.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
