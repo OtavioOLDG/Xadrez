7a8126f2a9fc949785385a2bab83ff8c2b7c6e4d
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2023-09-28 19:47:35 -0300

    Segundo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import board_game.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		Position position = new Position(1, 7);
+		
+		System.out.println(position);
 
 	}
 
